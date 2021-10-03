936ea246d92b4d2356252000cafd977b8d00c2da
Andre Celis <andreluiscelis@celisapp.com>
2021-10-02 23:23:19 -0300

    insercao de usuario com Post

--- src/main/java/com/celisapp/resource/UserResource.java
+++ src/main/java/com/celisapp/resource/UserResource.java
@@ -1,20 +1,24 @@
 package com.celisapp.resource;
 
+import java.net.URI;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.celisapp.domain.User;
 import com.celisapp.dto.UserDTO;
 import com.celisapp.service.UserService;
+
 
 @RestController
 @RequestMapping(value ="/users")
@@ -35,6 +39,15 @@ public class UserResource {
 		User user = service.findById(id);
 		UserDTO userDTO = new UserDTO(user);
 		return ResponseEntity.ok().body(userDTO) ;	
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert (@RequestBody UserDTO userDto){
+		User user = service.fromDTO(userDto);
+		 user = service.insert(user);
+		 URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				 .path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
--- src/main/java/com/celisapp/service/UserService.java
+++ src/main/java/com/celisapp/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.celisapp.domain.User;
+import com.celisapp.dto.UserDTO;
 import com.celisapp.repository.UserRepository;
 import com.celisapp.service.exception.ObjectNotFoundException;
 
@@ -24,6 +25,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repo.findById(id);
 		return user.orElseThrow(()-> new ObjectNotFoundException("Usuário não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO userDto) {
+		return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
 	}
 
 }
