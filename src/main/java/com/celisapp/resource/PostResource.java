671b555aaf6041f863aa80474fa65c3ac5e66c68
Andre Celis <andreluiscelis@celisapp.com>
2021-10-03 18:10:19 -0300

    adicionado comentarios aos posts

--- src/main/java/com/celisapp/resource/PostResource.java
+++ src/main/java/com/celisapp/resource/PostResource.java
@@ -32,36 +32,6 @@ public class PostResource {
 	public ResponseEntity<Post> findById (@PathVariable String id){
 		Post post = service.findById(id);
 		return ResponseEntity.ok().body(post) ;	
-	}
-	
-	@PostMapping
-	public ResponseEntity<Void> insert (@RequestBody UserDTO userDto){
-		User user = service.fromDTO(userDto);
-		 user = service.insert(user);
-		 URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-				 .path("/{id}").buildAndExpand(user.getId()).toUri();
-		return ResponseEntity.created(uri).build();
-	}
-	
-	@RequestMapping(value ="/{id}", method = RequestMethod.DELETE)
-	public ResponseEntity<Void> delete(@PathVariable String id){
-		service.delete(id);
-		return ResponseEntity.noContent().build();	
-	}
-	
-	@RequestMapping(value ="/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<UserDTO> update(@PathVariable String id , @RequestBody UserDTO userDto){
-		User user = service.fromDTO(userDto);
-		user.setId(id);
-		user = service.update(user);
-		UserDTO userDTO = new UserDTO(user);
-		return ResponseEntity.ok().body(userDTO);	
-	}
-	
-	@RequestMapping(value ="/{id}/posts", method = RequestMethod.GET)
-	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
-		User user = service.findById(id);
-		return ResponseEntity.ok().body(user.getPost()) ;	
-	}
+	}		
 
 }
