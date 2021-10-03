39492a42a05a77a8b8c965ae0e761f9748984aee
Andre Celis <andreluiscelis@celisapp.com>
2021-10-03 18:44:51 -0300

    consulta simples com query methos

--- src/main/java/com/celisapp/resource/PostResource.java
+++ src/main/java/com/celisapp/resource/PostResource.java
@@ -1,21 +1,16 @@
 package com.celisapp.resource;
-
-import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.celisapp.domain.Post;
-import com.celisapp.domain.User;
-import com.celisapp.dto.UserDTO;
+import com.celisapp.resource.util.URL;
 import com.celisapp.service.PostService;
 
 
@@ -32,6 +27,13 @@ public class PostResource {
 	public ResponseEntity<Post> findById (@PathVariable String id){
 		Post post = service.findById(id);
 		return ResponseEntity.ok().body(post) ;	
-	}		
+	}	
+	
+	@RequestMapping(value ="/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle (@RequestParam(value = "text",defaultValue = "" ) String text){
+		text = URL.decodeParans(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list) ;	
+	}
 
 }
--- src/main/java/com/celisapp/service/PostService.java
+++ src/main/java/com/celisapp/service/PostService.java
@@ -1,13 +1,12 @@
 package com.celisapp.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.celisapp.domain.Post;
-import com.celisapp.domain.User;
-import com.celisapp.dto.UserDTO;
 import com.celisapp.repository.PostRepository;
 import com.celisapp.service.exception.ObjectNotFoundException;
 
@@ -23,6 +22,10 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> post = repo.findById(id);
 		return post.orElseThrow(()-> new ObjectNotFoundException("Usuário não encontrado"));
+	}
+	
+	public List<Post> findByTitle(String title){
+		return repo.findByTitleContainingIgnoreCase(title);
 	}
 	
 
