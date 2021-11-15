b0ef63041605e37c35f99d96a8f2b1eaafd352c0
Andre Celis <andreluiscelis@celisapp.com>
2021-11-15 18:40:17 -0300

    removendo deleteAll de instatiation

--- src/main/java/com/celisapp/config/Instantiation.java
+++ src/main/java/com/celisapp/config/Instantiation.java
@@ -30,8 +30,6 @@ public class Instantiation implements CommandLineRunner {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
-		userRepository.deleteAll();
-		postRepository.deleteAll();
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
